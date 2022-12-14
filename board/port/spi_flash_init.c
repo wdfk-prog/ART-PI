2c1e0a2502ce777447bdbb3bffce3a6d1a0a6aca
wdfk-prog <1425075683@qq.com>
2022-12-14 16:55:20 +0800

    [board][port]更改时钟频率为100Mhz

--- board/port/spi_flash_init.c
+++ board/port/spi_flash_init.c
@@ -13,20 +13,36 @@
 #include <spi_flash.h>
 #include <drv_spi.h>
 
+#define SPI_DEVICE_NAME     "spi10"
+
 static int rt_flash_init(void)
 {
     extern rt_spi_flash_device_t rt_sfud_flash_probe(const char *spi_flash_dev_name, const char *spi_dev_name);
-    extern int fal_init(void);
 
     __HAL_RCC_GPIOA_CLK_ENABLE();
-    rt_hw_spi_device_attach("spi1", "spi10", GPIOA, GPIO_PIN_4);
+    rt_hw_spi_device_attach("spi1", SPI_DEVICE_NAME, GPIOA, GPIO_PIN_4);
 
     /* initialize SPI Flash device */
-    if (RT_NULL == rt_sfud_flash_probe("norflash0", "spi10"))
+    if (RT_NULL == rt_sfud_flash_probe("norflash0", SPI_DEVICE_NAME))
     {
         return -RT_ERROR;
     }
     
+    struct rt_spi_device *spi_dev;
+ /* 查找 spi 设备获取设备句柄 */
+    spi_dev = (struct rt_spi_device *)rt_device_find(SPI_DEVICE_NAME);
+    if (!spi_dev)
+    {
+        rt_kprintf("spi sample run failed! can't find %s device!\n", SPI_DEVICE_NAME);
+    }
+    else
+    {
+       struct rt_spi_configuration cfg;
+      cfg.data_width = 8;
+      cfg.mode = RT_SPI_MODE_0 | RT_SPI_MSB;
+      cfg.max_hz = 100 * 1000 *1000;//100MHZ
+      rt_spi_configure(spi_dev,&cfg);
+    }
     return RT_EOK;
 }
 INIT_ENV_EXPORT(rt_flash_init);
