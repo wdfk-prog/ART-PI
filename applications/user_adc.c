50700f4300a80050520b5254ab5b5fa537d16c31
wdfk-prog <1425075683@qq.com>
2022-12-20 13:02:14 +0800

    [app][adc]读取温度与参考电压数据使用adc框架 * 需使用Vref通道和Temp通道 * 使用18 19通道无效

--- applications/user_adc.c
+++ applications/user_adc.c
@@ -1,33 +1,41 @@
+#include <rtthread.h>
+#include <rtdevice.h>
 #include <main.h>
 
-extern ADC_HandleTypeDef hadc3;
+#define ADC_DEV_NAME        "adc3"      /* ADC 设备名称 */
+#define ADC_VREF_CHANNEL     ADC_CHANNEL_VREFINT - ADC_CHANNEL_0    /* ADC Vref 通道 */
+#define ADC_TEMP_CHANNEL     ADC_CHANNEL_TEMPSENSOR - ADC_CHANNEL_0 /* ADC Temp 通道 */
 
-static void adc_vol_sample(void)
+static int adc_vol_sample(void)
 {
     rt_uint32_t vref_value,temp_value;
     rt_uint16_t vref_mv,temp_mv; 
+
+    rt_adc_device_t adc_dev;
+    rt_err_t ret = RT_EOK;
+    
+    /* 查找设备 */
+    adc_dev = (rt_adc_device_t)rt_device_find(ADC_DEV_NAME);
+    if (adc_dev == RT_NULL)
+    {
+        rt_kprintf("adc sample run failed! can't find %s device!\n", ADC_DEV_NAME);
+        return RT_ERROR;
+    }
+
+    /* 使能设备 */
+    ret = rt_adc_enable(adc_dev, ADC_VREF_CHANNEL);
+    /* 读取采样值 */
+    vref_value = rt_adc_read(adc_dev, ADC_VREF_CHANNEL);
+    /* 关闭通道 */
+    ret = rt_adc_disable(adc_dev, ADC_VREF_CHANNEL);
+
+    /* 使能设备 */
+    ret = rt_adc_enable(adc_dev, ADC_TEMP_CHANNEL);
+    /* 读取采样值 */
+    temp_value = rt_adc_read(adc_dev, ADC_TEMP_CHANNEL);
+    /* 关闭通道 */
+    ret = rt_adc_disable(adc_dev, ADC_TEMP_CHANNEL);
   
-    // Reading ADC3 inputs VBAT, VRef, and Internal Temperature Sensor Channel.
-    if( HAL_OK != HAL_ADC_Start(&hadc3))
-    {
-      Error_Handler();
-    }
-    
-    if(HAL_OK != HAL_ADC_PollForConversion(&hadc3, 100))
-    {
-      Error_Handler();
-    }
-    vref_value = HAL_ADC_GetValue(&hadc3);
-    
-    // Measure Temperature
-    if(HAL_OK != HAL_ADC_PollForConversion(&hadc3, 100))
-    {
-      Error_Handler();
-    }
-    temp_value = HAL_ADC_GetValue(&hadc3);
-    
-    HAL_ADC_Stop(&hadc3);
-    
     rt_kprintf("VRef  is %u.\n", vref_value);
     rt_kprintf("Temp is %u.\n" , temp_value);
     
@@ -38,6 +46,8 @@ static void adc_vol_sample(void)
     rt_kprintf("%ld, and %d are Temperature in Degree C calculated using two different formulas.\n",
     __HAL_ADC_CALC_TEMPERATURE(vref_mv, temp_value, ADC_RESOLUTION_16B), 
     __HAL_ADC_CALC_TEMPERATURE_TYP_PARAMS(2000, 620, 30, vref_mv, temp_value, ADC_RESOLUTION_16B));
+
+    return RT_EOK;
 }
 /* 导出到 msh 命令列表中 */
 MSH_CMD_EXPORT(adc_vol_sample, adc voltage convert sample);
