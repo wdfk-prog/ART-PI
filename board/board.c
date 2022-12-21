40762b73839e5b09fc7aa6a2c5ba0acd033c8ffb
wdfk-prog <1425075683@qq.com>
2022-12-21 15:00:32 +0800

    修改get_nvic_irq函数位置

--- board/board.c
+++ board/board.c
@@ -23,3 +23,24 @@ static int vtor_config(void)
     return 0;
 }
 INIT_BOARD_EXPORT(vtor_config);
+/**
+  * @brief  None.
+  * @param  None.
+  * @retval None.
+  * @note   None.
+*/
+void get_nvic_irq()
+{
+  rt_kprintf("ldx E P A Priotity\n");
+  for (rt_uint8_t i = 0; i < 255; i++)
+  {
+      if(NVIC_GetEnableIRQ(i))
+      {
+          rt_kprintf("%3d 1",i);
+          NVIC_GetPendingIRQ(i) ? rt_kprintf(" 1") : rt_kprintf(" 0");
+          NVIC_GetActive(i) ? rt_kprintf(" 1") : rt_kprintf(" 0");
+          rt_kprintf(" %02d\n",NVIC_GetPriority(i));
+      }
+  }
+}
+MSH_CMD_EXPORT(get_nvic_irq, get all enable NVIC_IRQ);
