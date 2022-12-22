0f5226fb927ab8e40466a2cfbf5978b649aa90b8
wdfk-prog <1425075683@qq.com>
2022-12-22 15:36:35 +0800

    [board][CubeMX]配置WDT功能

--- board/CubeMX_Config/Core/Src/main.c
+++ board/CubeMX_Config/Core/Src/main.c
@@ -23,6 +23,7 @@
 #include "main.h"
 #include "adc.h"
 #include "dma.h"
+#include "iwdg.h"
 #include "rtc.h"
 #include "spi.h"
 #include "usart.h"
@@ -76,6 +77,7 @@ extern int get_vref_temp(void);
 int main(void)
 {
     Version();
+    rt_thread_mdelay(500);//初始化后延时等待ADC稳定
     get_vref_temp();
     rt_pin_mode(LED_PIN, PIN_MODE_OUTPUT);
     while(1)
@@ -130,12 +132,13 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSE
-                              |RCC_OSCILLATORTYPE_LSE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSI
+                              |RCC_OSCILLATORTYPE_HSE|RCC_OSCILLATORTYPE_LSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.LSEState = RCC_LSE_ON;
   RCC_OscInitStruct.HSIState = RCC_HSI_DIV1;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 5;
