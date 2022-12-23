f55aef26f16bd899c5d08ce3c296cfb6848c4d13
wdfk-prog <1425075683@qq.com>
2022-12-23 17:55:36 +0800

    [ulog][Ymodem]更新sy支持路径传输

--- rt-thread/components/utilities/ymodem/ry_sy.c
+++ rt-thread/components/utilities/ymodem/ry_sy.c
@@ -30,6 +30,16 @@ struct custom_ctx
     char fpath[DFS_PATH_MAX];
 };
 
+static const char *_get_path_lastname(const char *path)
+{
+    char *ptr;
+    if ((ptr = (char *)strrchr(path, '/')) == NULL)
+        return path;
+
+    /* skip the '/' then return */
+    return ++ptr;
+}
+
 static enum rym_code _rym_recv_begin(
     struct rym_ctx *ctx,
     rt_uint8_t *buf,
@@ -110,6 +120,7 @@ static enum rym_code _rym_send_begin(
     struct stat file_buf;
     char insert_0 = '\0';
     rt_err_t err;
+    const char *fullpath;
 
     cctx->fd = open(cctx->fpath, O_RDONLY);
     if (cctx->fd < 0)
@@ -125,7 +136,9 @@ static enum rym_code _rym_send_begin(
         rt_kprintf("error open file.\n");
         return RYM_ERR_FILE;
     }
-    rt_sprintf((char *)buf, "%s%c%d", (char *) & (cctx->fpath[1]), insert_0, file_buf.st_size);
+
+    fullpath = dfs_normalize_path(RT_NULL, _get_path_lastname(cctx->fpath));
+    rt_sprintf((char *)buf, "%s%c%d", fullpath, insert_0, file_buf.st_size);
 
     return RYM_CODE_SOH;
 }
