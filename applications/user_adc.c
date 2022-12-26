6cb06c479e0d3fe8c4890c1d15561892b28d0fcf
wdfk-prog <1425075683@qq.com>
2022-12-26 10:48:13 +0800

    [app][adc]更新支持使用设备框架或dma驱动

--- applications/user_adc.c
+++ applications/user_adc.c
@@ -22,6 +22,7 @@
 #define LOG_LVL              DBG_INFO
 #include <ulog.h>
 /* Private typedef -----------------------------------------------------------*/
+#ifdef USER_USING_ADC
 /** 
   * @brief ADC1通道值定义
   */  
@@ -146,3 +147,57 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
     }
 }
 #endif /* (REAL_TIME_REFRESH == 0) */
+#else
+#define ADC_DEV_NAME        "adc3"      /* ADC 设备名称 */
+#define ADC_VREF_CHANNEL     ADC_CHANNEL_VREFINT - ADC_CHANNEL_0    /* ADC Vref 通道 */
+#define ADC_TEMP_CHANNEL     ADC_CHANNEL_TEMPSENSOR - ADC_CHANNEL_0 /* ADC Temp 通道 */
+/**
+  * @brief  获取 MCU内部参考电压和温度
+  * @param  None
+  * @retval int 
+  * @note   None
+*/
+int vref_temp_get(void)
+{
+    rt_uint32_t vref_value,temp_value;
+    rt_uint16_t vref_mv,temp_mv; 
+
+    rt_adc_device_t adc_dev;
+    rt_err_t ret = RT_EOK;
+    
+    /* 查找设备 */
+    adc_dev = (rt_adc_device_t)rt_device_find(ADC_DEV_NAME);
+    if (adc_dev == RT_NULL)
+    {
+        rt_kprintf("adc sample run failed! can't find %s device!\n", ADC_DEV_NAME);
+        return RT_ERROR;
+    }
+
+    /* 使能设备 */
+    ret = rt_adc_enable(adc_dev, ADC_VREF_CHANNEL);
+    /* 读取采样值 */
+    vref_value = rt_adc_read(adc_dev, ADC_VREF_CHANNEL);
+    /* 关闭通道 */
+    ret = rt_adc_disable(adc_dev, ADC_VREF_CHANNEL);
+
+    /* 使能设备 */
+    ret = rt_adc_enable(adc_dev, ADC_TEMP_CHANNEL);
+    /* 读取采样值 */
+    temp_value = rt_adc_read(adc_dev, ADC_TEMP_CHANNEL);
+    /* 关闭通道 */
+    ret = rt_adc_disable(adc_dev, ADC_TEMP_CHANNEL);
+  
+    rt_kprintf("Vref  is %u.\n", vref_value);
+    rt_kprintf("Temp is %u.\n" , temp_value);
+    
+    // Calculating Vref voltage
+    vref_mv = __HAL_ADC_CALC_VREFANALOG_VOLTAGE(vref_value, ADC_RESOLUTION_16B);
+    rt_kprintf("Vref voltage is %u mV.\n", vref_mv);
+    // Calculate Temperature
+    rt_kprintf("%d are Temperature in Degree C.\n",
+    __HAL_ADC_CALC_TEMPERATURE(vref_mv, temp_value, ADC_RESOLUTION_16B));
+    return RT_EOK;
+}
+/* 导出到 msh 命令列表中 */
+MSH_CMD_EXPORT(vref_temp_get, get MCU vref and temp);
+#endif /* USER_USING_ADC */
