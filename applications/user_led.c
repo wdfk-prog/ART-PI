842956142d25e720f23f0ed23bb5538ce00cd5d9
wdfk-prog <1425075683@qq.com>
2023-02-01 20:44:23 +0800

    格式代码

--- applications/user_led.c
+++ applications/user_led.c
@@ -1,17 +1,17 @@
 /*
  * @file user_led.c
- * @brief 
+ * @brief
  * @author wdfk-prog (1425075683@qq.com)
  * @version 1.0
  * @date 2023-02-01
- * 
- * @copyright Copyright (c) 2023  
- * 
- * @attention 
+ *
+ * @copyright Copyright (c) 2023
+ *
+ * @attention
  * @par 修改日志:
  * Date       Version Author  Description
  * 2023-02-01 1.0     wdfk-prog    first version
-*/
+ */
 /* Includes ------------------------------------------------------------------*/
 
 /* Private includes ----------------------------------------------------------*/
@@ -35,22 +35,22 @@ static rt_timer_t timer1;
 /* Private function prototypes -----------------------------------------------*/
 static void Led_CallBack(iLed_t *handle);
 /**
-  * @brief  置一
-  * @param  None
-  * @retval None
-  * @note   None
-*/
+ * @brief  置一
+ * @param  None
+ * @retval None
+ * @note   None
+ */
 void led_set()
 {
     HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
     HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
 }
 /**
-  * @brief  置零
-  * @param  None
-  * @retval None
-  * @note   None
-*/
+ * @brief  置零
+ * @param  None
+ * @retval None
+ * @note   None
+ */
 void led_reset(void)
 {
     HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
@@ -59,7 +59,7 @@ void led_reset(void)
 /**
  * @brief  定时器超时回调
  * @note   led计数与执行
- * @param  *parameter: 
+ * @param  *parameter:
  * @retval None
  */
 static void timeout1(void *parameter)
@@ -85,7 +85,7 @@ static int User_Led_Init(void)
                              1,
                              RT_TIMER_FLAG_PERIODIC);
     /* 启动定时器 1 */
-    if (timer1 != RT_NULL) 
+    if (timer1 != RT_NULL)
         rt_timer_start(timer1);
     return 0;
 }
