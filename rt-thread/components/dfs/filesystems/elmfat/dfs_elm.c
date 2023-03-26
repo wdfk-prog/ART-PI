dbf24c16045e65ebcfdd85b94d39c91ffa5ecbd7
wdfk-prog <1425075683@qq.com>
2023-03-26 20:20:40 +0800

    ✨ feat(components/dfs/filesystems/elmfat/dfs_elm.c): add st_blocks su… (#7091)

--- rt-thread/components/dfs/filesystems/elmfat/dfs_elm.c
+++ rt-thread/components/dfs/filesystems/elmfat/dfs_elm.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006-2023, RT-Thread Development Team
+ * Copyright (c) 2006-2021, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -28,6 +28,13 @@
 
 #include <dfs_fs.h>
 #include <dfs_file.h>
+
+#undef SS
+#if FF_MAX_SS == FF_MIN_SS
+#define SS(fs) ((UINT)FF_MAX_SS) /* Fixed sector size */
+#else
+#define SS(fs) ((fs)->ssize) /* Variable sector size */
+#endif
 
 static rt_device_t disk[FF_VOLUMES] = {0};
 
@@ -342,6 +349,7 @@ int dfs_elm_open(struct dfs_fd *file)
             return -ENOENT;
         }
         file->pos = 0;
+        //return 0;
     }
 
     if (fs == NULL)
@@ -750,8 +758,11 @@ int dfs_elm_rename(struct dfs_filesystem *fs, const char *oldpath, const char *n
 
 int dfs_elm_stat(struct dfs_filesystem *fs, const char *path, struct stat *st)
 {
+    FATFS  *f;
     FILINFO file_info;
     FRESULT result;
+
+    f = (FATFS *)fs->data;
 
 #if FF_VOLUMES > 1
     int vol;
@@ -792,7 +803,16 @@ int dfs_elm_stat(struct dfs_filesystem *fs, const char *path, struct stat *st)
             st->st_mode &= ~(S_IWUSR | S_IWGRP | S_IWOTH);
 
         st->st_size  = file_info.fsize;
-
+        st->st_blksize = f->csize * SS(f);
+        if (file_info.fattrib & AM_ARC)
+        {
+            st->st_blocks = file_info.fsize ? ((file_info.fsize - 1) / SS(f) / f->csize + 1) : 0;
+            st->st_blocks *= (st->st_blksize / 512);  // man say st_blocks is number of 512B blocks allocated
+        }
+        else
+        {
+            st->st_blocks = f->csize;
+        }
         /* get st_mtime. */
         {
             struct tm tm_file;
