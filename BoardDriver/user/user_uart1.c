f008774a6eed3ffc98a222d7e6e18005bbb66bb6
wdfk-prog <1425075683@qq.com>
2024-06-12 22:08:26 +0800

    feat:使用串口V2

--- BoardDriver/user/user_uart1.c
+++ BoardDriver/user/user_uart1.c
@@ -1,130 +1,130 @@
-///**
-// * @file user_uart1.c
-// * @brief
-// * @author HLY (1425075683@qq.com)
-// * @version 1.0
-// * @date 2022-12-26
-// * @copyright Copyright (c) 2022
-// * @attention
-// * @par 修改日志:
-// * Date       Version Author  Description
-// * 2022-12-26 1.0     HLY     first version
-// */
-///* Includes ------------------------------------------------------------------*/
-//#include <rtthread.h>
-//#include <rtdevice.h>
-///* Private includes ----------------------------------------------------------*/
+/**
+ * @file user_uart1.c
+ * @brief
+ * @author HLY (1425075683@qq.com)
+ * @version 1.0
+ * @date 2022-12-26
+ * @copyright Copyright (c) 2022
+ * @attention
+ * @par 修改日志:
+ * Date       Version Author  Description
+ * 2022-12-26 1.0     HLY     first version
+ */
+/* Includes ------------------------------------------------------------------*/
+#include <rtthread.h>
+#include <rtdevice.h>
+/* Private includes ----------------------------------------------------------*/
 
-///* Private typedef -----------------------------------------------------------*/
-///* 串口接收消息结构 */
-//struct rx_msg
-//{
-//    rt_device_t dev;
-//    rt_size_t size;
-//};
-///* Private define ------------------------------------------------------------*/
-//#define UART_NAME "uart1" /* 串口设备名称 */
-///* Private macro -------------------------------------------------------------*/
+/* Private typedef -----------------------------------------------------------*/
+/* 串口接收消息结构 */
+struct rx_msg
+{
+    rt_device_t dev;
+    rt_size_t size;
+};
+/* Private define ------------------------------------------------------------*/
+#define UART_NAME "uart1" /* 串口设备名称 */
+/* Private macro -------------------------------------------------------------*/
 
-///* Private variables ---------------------------------------------------------*/
-///* 串口设备句柄 */
-//static rt_device_t serial;
-///* 消息队列控制块 */
-//static struct rt_messagequeue rx_mq;
-///* Private function prototypes -----------------------------------------------*/
-///**
-// * @brief  串口接收函数
-// * @param  dev
-// * @param  size
-// * @retval rt_err_t
-// */
-//static rt_err_t uart_input(rt_device_t dev, rt_size_t size)
-//{
-//    struct rx_msg msg;
-//    rt_err_t result;
-//    msg.dev = dev;
-//    msg.size = size;
+/* Private variables ---------------------------------------------------------*/
+/* 串口设备句柄 */
+static rt_device_t serial;
+/* 消息队列控制块 */
+static struct rt_messagequeue rx_mq;
+/* Private function prototypes -----------------------------------------------*/
+/**
+ * @brief  串口接收函数
+ * @param  dev
+ * @param  size
+ * @retval rt_err_t
+ */
+static rt_err_t uart_input(rt_device_t dev, rt_size_t size)
+{
+    struct rx_msg msg;
+    rt_err_t result;
+    msg.dev = dev;
+    msg.size = size;
 
-//    result = rt_mq_send(&rx_mq, &msg, sizeof(msg));
-//    if (result == -RT_EFULL)
-//    {
-//        /* 消息队列满 */
-//        rt_kprintf("message queue full！\n");
-//    }
-//    return result;
-//}
-///**
-// * @brief 串口接收线程
-// * @param  parameter
-// */
-//static void serial_thread_entry(void *parameter)
-//{
-//    struct rx_msg msg;
-//    rt_err_t result;
-//    rt_uint32_t rx_length;
-//    static char rx_buffer[BSP_UART1_RX_BUFSIZE + 1];
+    result = rt_mq_send(&rx_mq, &msg, sizeof(msg));
+    if (result == -RT_EFULL)
+    {
+        /* 消息队列满 */
+        rt_kprintf("message queue full！\n");
+    }
+    return result;
+}
+/**
+ * @brief 串口接收线程
+ * @param  parameter
+ */
+static void serial_thread_entry(void *parameter)
+{
+    struct rx_msg msg;
+    rt_err_t result;
+    rt_uint32_t rx_length;
+    static char rx_buffer[BSP_UART1_RX_BUFSIZE + 1];
 
-//    while (1)
-//    {
-//        rt_memset(&msg, 0, sizeof(msg));
-//        /* 从消息队列中读取消息 */
-//        result = rt_mq_recv(&rx_mq, &msg, sizeof(msg), RT_WAITING_FOREVER);
-//        if (result == RT_EOK)
-//        {
-//            /* 从串口读取数据 */
-//            rx_length = rt_device_read(msg.dev, 0, rx_buffer, msg.size);
-//            rx_buffer[rx_length] = '\0';
-//            /* 通过串口设备 serial 输出读取到的消息 */
-//            rt_device_write(serial, 0, rx_buffer, rx_length);
-//            /* 打印数据 */
-//            rt_kprintf("%s\n", rx_buffer);
-//        }
-//    }
-//}
-///**
-// * @brief 串口1初始化
-// * @retval int
-// */
-//static int uart1_init(void)
-//{
-//    rt_err_t ret = RT_EOK;
-//    static char msg_pool[256];
-//    char str[] = "hello RT-Thread!\r\n";
+    while (1)
+    {
+        rt_memset(&msg, 0, sizeof(msg));
+        /* 从消息队列中读取消息 */
+        result = rt_mq_recv(&rx_mq, &msg, sizeof(msg), RT_WAITING_FOREVER);
+        if (result == RT_EOK)
+        {
+            /* 从串口读取数据 */
+            rx_length = rt_device_read(msg.dev, 0, rx_buffer, msg.size);
+            rx_buffer[rx_length] = '\0';
+            /* 通过串口设备 serial 输出读取到的消息 */
+            rt_device_write(serial, 0, rx_buffer, rx_length);
+            /* 打印数据 */
+            rt_kprintf("%s\n", rx_buffer);
+        }
+    }
+}
+/**
+ * @brief 串口1初始化
+ * @retval int
+ */
+static int uart1_init(void)
+{
+    rt_err_t ret = RT_EOK;
+    static char msg_pool[256];
+    char str[] = "hello RT-Thread!\r\n";
 
-//    /* 查找串口设备 */
-//    serial = rt_device_find(UART_NAME);
-//    if (!serial)
-//    {
-//        rt_kprintf("find %s failed!\n", UART_NAME);
-//        return RT_ERROR;
-//    }
+    /* 查找串口设备 */
+    serial = rt_device_find(UART_NAME);
+    if (!serial)
+    {
+        rt_kprintf("find %s failed!\n", UART_NAME);
+        return RT_ERROR;
+    }
 
-//    /* 初始化消息队列 */
-//    rt_mq_init(&rx_mq, "rx_mq",
-//               msg_pool,              /* 存放消息的缓冲区 */
-//               sizeof(struct rx_msg), /* 一条消息的最大长度 */
-//               sizeof(msg_pool),      /* 存放消息的缓冲区大小 */
-//               RT_IPC_FLAG_FIFO);     /* 如果有多个线程等待，按照先来先得到的方法分配消息 */
+    /* 初始化消息队列 */
+    rt_mq_init(&rx_mq, "rx_mq",
+               msg_pool,              /* 存放消息的缓冲区 */
+               sizeof(struct rx_msg), /* 一条消息的最大长度 */
+               sizeof(msg_pool),      /* 存放消息的缓冲区大小 */
+               RT_IPC_FLAG_FIFO);     /* 如果有多个线程等待，按照先来先得到的方法分配消息 */
 
-//    /* 以 DMA 接收及轮询发送方式打开串口设备 */
-//    rt_device_open(serial, RT_DEVICE_FLAG_RX_NON_BLOCKING | RT_DEVICE_FLAG_TX_BLOCKING);
-//    /* 设置接收回调函数 */
-//    rt_device_set_rx_indicate(serial, uart_input);
-//    /* 发送字符串 */
-//    rt_device_write(serial, 0, str, (sizeof(str) - 1));
+    /* 以 DMA 接收及轮询发送方式打开串口设备 */
+    rt_device_open(serial, RT_DEVICE_FLAG_RX_NON_BLOCKING | RT_DEVICE_FLAG_TX_BLOCKING);
+    /* 设置接收回调函数 */
+    rt_device_set_rx_indicate(serial, uart_input);
+    /* 发送字符串 */
+    rt_device_write(serial, 0, str, (sizeof(str) - 1));
 
-//    /* 创建 serial 线程 */
-//    rt_thread_t thread = rt_thread_create("uart1", serial_thread_entry, RT_NULL, 1024, 25, 10);
-//    /* 创建成功则启动线程 */
-//    if (thread != RT_NULL)
-//    {
-//        rt_thread_startup(thread);
-//    }
-//    else
-//    {
-//        ret = RT_ERROR;
-//    }
+    /* 创建 serial 线程 */
+    rt_thread_t thread = rt_thread_create("uart1", serial_thread_entry, RT_NULL, 1024, 25, 10);
+    /* 创建成功则启动线程 */
+    if (thread != RT_NULL)
+    {
+        rt_thread_startup(thread);
+    }
+    else
+    {
+        ret = RT_ERROR;
+    }
 
-//    return ret;
-//}
-//INIT_APP_EXPORT(uart1_init);
+    return ret;
+}
+INIT_APP_EXPORT(uart1_init);
--- rtconfig.h
+++ rtconfig.h
@@ -1,6 +1,9 @@
 #ifndef RT_CONFIG_H__
 #define RT_CONFIG_H__
 
+/* Automatically generated file; DO NOT EDIT. */
+/* RT-Thread Configuration */
+
 #define RT_STUDIO_BUILT_IN
 
 /* RT-Thread Kernel */
@@ -23,11 +26,9 @@
 
 /* kservice optimization */
 
-/* end of kservice optimization */
 
 /* klibc optimization */
 
-/* end of klibc optimization */
 #define RT_USING_DEBUG
 #define RT_DEBUGING_COLOR
 #define RT_DEBUGING_CONTEXT
@@ -39,7 +40,6 @@
 #define RT_USING_EVENT
 #define RT_USING_MAILBOX
 #define RT_USING_MESSAGEQUEUE
-/* end of Inter-Thread communication */
 
 /* Memory Management */
 
@@ -52,7 +52,6 @@
 #define RT_USING_MEMTRACE
 #define RT_USING_HEAP_ISR
 #define RT_USING_HEAP
-/* end of Memory Management */
 #define RT_USING_DEVICE
 #define RT_USING_DEVICE_OPS
 #define RT_USING_CONSOLE
@@ -60,7 +59,6 @@
 #define RT_CONSOLE_DEVICE_NAME "uart4"
 #define RT_VER_NUM 0x50200
 #define RT_BACKTRACE_LEVEL_MAX_NR 32
-/* end of RT-Thread Kernel */
 #define RT_USING_CACHE
 #define RT_USING_CPU_FFS
 #define ARCH_ARM
@@ -112,10 +110,8 @@
 #define RT_DFS_ELM_MAX_SECTOR_SIZE 512
 #define RT_DFS_ELM_REENTRANT
 #define RT_DFS_ELM_MUTEX_TIMEOUT 3000
-/* end of elm-chan's FatFs, Generic FAT Filesystem Module */
 #define RT_USING_DFS_DEVFS
 #define RT_USING_DFS_ROMFS
-/* end of DFS: device virtual file system */
 #define RT_USING_FAL
 #define FAL_DEBUG_CONFIG
 #define FAL_DEBUG 1
@@ -128,8 +124,8 @@
 #define RT_USING_DEVICE_IPC
 #define RT_UNAMED_PIPE_NUMBER 64
 #define RT_USING_SERIAL
-#define RT_USING_SERIAL_V1
-#define RT_SERIAL_RB_BUFSZ 64
+#define RT_USING_SERIAL_V2
+#define RT_SERIAL_USING_DMA
 #define RT_USING_MTD_NOR
 #define RT_USING_RTC
 #define RT_USING_SDIO
@@ -156,8 +152,6 @@
 #define _RT_USB_DEVICE_MSTORAGE
 #define RT_USB_DEVICE_MSTORAGE
 #define RT_USB_MSTORAGE_DISK_NAME "sd0"
-/* end of Using USB */
-/* end of Device Drivers */
 
 /* C/C++ and POSIX layer */
 
@@ -169,8 +163,6 @@
 #define RT_LIBC_TZ_DEFAULT_HOUR 8
 #define RT_LIBC_TZ_DEFAULT_MIN 0
 #define RT_LIBC_TZ_DEFAULT_SEC 0
-/* end of Timezone and Daylight Saving Time */
-/* end of ISO-ANSI C layer */
 
 /* POSIX (Portable Operating System Interface) layer */
 
@@ -180,17 +172,12 @@
 
 /* Socket is in the 'Network' category */
 
-/* end of Interprocess Communication (IPC) */
-/* end of POSIX (Portable Operating System Interface) layer */
-/* end of C/C++ and POSIX layer */
 
 /* Network */
 
-/* end of Network */
 
 /* Memory protection */
 
-/* end of Memory protection */
 
 /* Utilities */
 
@@ -217,15 +204,12 @@
 #define ULOG_TIME_USING_TIMESTAMP
 #define ULOG_OUTPUT_LEVEL
 #define ULOG_OUTPUT_TAG
-/* end of log format */
 #define ULOG_BACKEND_USING_CONSOLE
 #define ULOG_BACKEND_USING_FILE
 #define ULOG_USING_FILTER
 #define RT_USING_UTEST
 #define UTEST_THR_STACK_SIZE 4096
 #define UTEST_THR_PRIORITY 20
-/* end of Utilities */
-/* end of RT-Thread Components */
 
 /* RT-Thread online packages */
 
@@ -236,55 +220,40 @@
 
 /* Marvell WiFi */
 
-/* end of Marvell WiFi */
 
 /* Wiced WiFi */
 
-/* end of Wiced WiFi */
 
 /* CYW43012 WiFi */
 
-/* end of CYW43012 WiFi */
 
 /* BL808 WiFi */
 
-/* end of BL808 WiFi */
 
 /* CYW43439 WiFi */
 
-/* end of CYW43439 WiFi */
-/* end of Wi-Fi */
 
 /* IoT Cloud */
 
-/* end of IoT Cloud */
-/* end of IoT - internet of things */
 
 /* security packages */
 
-/* end of security packages */
 
 /* language packages */
 
 /* JSON: JavaScript Object Notation, a lightweight data-interchange format */
 
-/* end of JSON: JavaScript Object Notation, a lightweight data-interchange format */
 
 /* XML: Extensible Markup Language */
 
-/* end of XML: Extensible Markup Language */
-/* end of language packages */
 
 /* multimedia packages */
 
 /* LVGL: powerful and easy-to-use embedded GUI library */
 
-/* end of LVGL: powerful and easy-to-use embedded GUI library */
 
 /* u8g2: a monochrome graphic library */
 
-/* end of u8g2: a monochrome graphic library */
-/* end of multimedia packages */
 
 /* tools packages */
 
@@ -296,7 +265,6 @@
 #define PKG_CMBACKTRACE_VER_NUM 0x99999
 #define PKG_USING_CPU_USAGE
 #define PKG_USING_CPU_USAGE_LATEST_VERSION
-/* end of tools packages */
 
 /* system packages */
 
@@ -323,19 +291,15 @@
 #define RT_VSNPRINTF_FULL_REPLACING_VSPRINTF
 #define RT_VSNPRINTF_FULL_REPLACING_VSNPRINTF
 #define PKG_USING_RT_VSNPRINTF_FULL_LATEST_VERSION
-/* end of enhanced kernel services */
 
 /* acceleration: Assembly language or algorithmic acceleration packages */
 
-/* end of acceleration: Assembly language or algorithmic acceleration packages */
 
 /* CMSIS: ARM Cortex-M Microcontroller Software Interface Standard */
 
-/* end of CMSIS: ARM Cortex-M Microcontroller Software Interface Standard */
 
 /* Micrium: Micrium software products porting for RT-Thread */
 
-/* end of Micrium: Micrium software products porting for RT-Thread */
 #define PKG_USING_LITTLEFS
 #define PKG_USING_LITTLEFS_LATEST_VERSION
 #define LFS_READ_SIZE 256
@@ -357,44 +321,33 @@
 #define SYSWATCH_WDT_NAME "wdt"
 #define SYSWATCH_WDT_TIMEOUT 5
 #define PKG_USING_SYSWATCH_LATEST_VERSION
-/* end of system packages */
 
 /* peripheral libraries and drivers */
 
 /* sensors drivers */
 
-/* end of sensors drivers */
 
 /* touch drivers */
 
-/* end of touch drivers */
 
 /* Kendryte SDK */
 
-/* end of Kendryte SDK */
-/* end of peripheral libraries and drivers */
 
 /* AI packages */
 
-/* end of AI packages */
 
 /* Signal Processing and Control Algorithm Packages */
 
-/* end of Signal Processing and Control Algorithm Packages */
 
 /* miscellaneous packages */
 
 /* project laboratory */
 
-/* end of project laboratory */
-
 /* samples: kernel and components samples */
 
-/* end of samples: kernel and components samples */
 
 /* entertainment: terminal games and other interesting software packages */
 
-/* end of entertainment: terminal games and other interesting software packages */
 #define PKG_USING_MFBD
 #define PKG_MFBD_BTN_CODE_SIZE_2BYTE
 #define PKG_MFBD_BTN_CODE_SIZE 2
@@ -409,53 +362,40 @@
 #define PKG_MFBD_VER_NUM 0x999999
 #define PKG_USING_GET_IRQ_PRIORITY
 #define PKG_USING_GET_IRQ_PRIORITY_LATEST_VERSION
-/* end of miscellaneous packages */
 
 /* Arduino libraries */
 
 
 /* Projects and Demos */
 
-/* end of Projects and Demos */
 
 /* Sensors */
 
-/* end of Sensors */
 
 /* Display */
 
-/* end of Display */
 
 /* Timing */
 
-/* end of Timing */
 
 /* Data Processing */
 
-/* end of Data Processing */
 
 /* Data Storage */
 
 /* Communication */
 
-/* end of Communication */
 
 /* Device Control */
 
-/* end of Device Control */
 
 /* Other */
 
-/* end of Other */
 
 /* Signal IO */
 
-/* end of Signal IO */
 
 /* Uncategorized */
-
-/* end of Arduino libraries */
-/* end of RT-Thread online packages */
 
 /* Hardware Drivers Config */
 
@@ -468,7 +408,6 @@
 
 /* Board extended module */
 
-/* end of Board extended module */
 
 /* Onboard Peripheral */
 
@@ -477,7 +416,6 @@
 #define BSP_USING_FS
 #define BSP_USING_SDCARD_FS
 #define BSP_USING_SPI_FLASH_FS
-/* end of Onboard Peripheral */
 
 /* On-chip Peripheral */
 
@@ -502,23 +440,17 @@
 #define BSP_USING_USBD
 #define BSP_USING_ONCHIP_RTC
 #define BSP_USING_WDT
-/* end of On-chip Peripheral */
-/* end of Hardware Drivers Config */
 
 /* External Libraries */
 
-/* end of External Libraries */
 
 /* User Driver Config */
 
 /* USER ADC Drivers */
 
 #define USER_USING_ADC
-/* end of USER ADC Drivers */
 
 /* CANFESTIVAL MASTER402 */
 
-/* end of CANFESTIVAL MASTER402 */
-/* end of User Driver Config */
 
 #endif
